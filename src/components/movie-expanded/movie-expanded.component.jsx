c87a48f05d8d9c0ad0a3b07ac830c71b9e3904e0
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-01-14 15:59:50 +0200

    small ui improvements

--- src/components/movie-expanded/movie-expanded.component.jsx
+++ src/components/movie-expanded/movie-expanded.component.jsx
@@ -15,129 +15,129 @@ import YouTube from 'react-youtube';
 
 
 const MovieExpanded = ({movie, movieDetails}) => {
-        const useStyles = makeStyles((theme) => ({
-            root: {
-                maxWidth: "100%",
-                maxHeight: "100%",
-                overflow: 'auto',
-                width: '100%',
-                backgroundColor: 'white',
-                position: 'relative'
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            maxWidth: "100%",
+            maxHeight: "100%",
+            overflow: 'auto',
+            width: '100%',
+            backgroundColor: 'white',
+            position: 'relative'
+        },
+        genres: {
+            fontSize: '1rem',
+            '@media (max-width:320px)': {
+                fontSize: '0.75rem',
             },
-            genres: {
-                fontSize: '1rem',
-                '@media (max-width:320px)': {
-                    fontSize: '0.75rem',
-                },
-            },
-            heading: {
-                fontSize: theme.typography.pxToRem(15),
-                fontWeight: theme.typography.fontWeightRegular,
-            },
-            media: {
-                height: 0,
-                paddingTop: '36.25%', // 16:9
-                backgroundSize: "contain",
-            },
-            expand: {
-                transform: 'rotate(0deg)',
-                marginLeft: 'auto',
-                color: "white",
-                background: "#064789",
-                transition: theme.transitions.create('transform', {
-                    duration: theme.transitions.duration.shortest,
-                }),
-            },
-            expandOpen: {
-                transform: 'rotate(180deg)',
-                display: 'none'
-            },
-            imageWrapper: {
-                display: "flex",
-                alignItems: 'center',
-                justifyContent: 'center',
-                maxHeight: "300px"
-            },
-            image: {
-                width: "100%",
-                height: "100%",
-            },
-            trailer: {
-                width: "100%",
-                height: "520px",
-                margin: "auto",
-                '@media (max-width:380px)': {
-                    height: '200px',
-                    width: "100%"
-                }
+        },
+        heading: {
+            fontSize: theme.typography.pxToRem(15),
+            fontWeight: theme.typography.fontWeightRegular,
+        },
+        media: {
+            height: 0,
+            paddingTop: '36.25%', // 16:9
+            backgroundSize: "contain",
+        },
+        expand: {
+            transform: 'rotate(0deg)',
+            marginLeft: 'auto',
+            color: "white",
+            background: "#064789",
+            transition: theme.transitions.create('transform', {
+                duration: theme.transitions.duration.shortest,
+            }),
+        },
+        expandOpen: {
+            transform: 'rotate(180deg)',
+            display: 'none'
+        },
+        imageWrapper: {
+            display: "flex",
+            alignItems: 'center',
+            justifyContent: 'center',
+            maxHeight: "300px"
+        },
+        image: {
+            width: "100%",
+            height: "100%",
+        },
+        trailer: {
+            width: "100%",
+            height: "530px",
+            margin: "auto",
+            '@media (max-width:420px)': {
+                height: '200px',
+                width: "100%"
             }
-        }));
-        const classes = useStyles();
+        }
+    }));
+    const classes = useStyles();
 
 
-        return (
-            <>
-                {movie && <div>
-                        {/*<CardMedia*/}
-                        {/*    className={classes.media}*/}
-                        {/*    image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}*/}
-                        {/*    title={`${movie.original_title}`}*/}
-                        {/*/>*/}
-                        <CardContent>
-                            {/*<div className={classes.trailer}>*/}
-                            {/*<iframe width="100%" height="100%"*/}
-                            {/*        src={`https://www.youtube.com/embed/59fc956792514113bb02d590`}>*/}
-                            {/*</iframe>*/}
-                            <YouTube videoId={movieDetails.videos.results[0].key} className={classes.trailer}/>
-                            {/*</div>*/}
-                                <Box fontStyle="italic" fontSize={"1.5rem"}>
-                                    <Typography variant="p" component="p" align="center">
-                                        {movieDetails.tagline}
-                                    </Typography>
-                                </Box>
-                                <Typography variant="p" component="p" align="center">
-                                    {movieDetails.overview}
-                                </Typography>
-                            <CardActions disableSpacing>
-                                <div className="d-flex align-items-center justify-content-around card-bottom">
-                                    <Typography variant="body4" color="textSecondary" component="p">
-                                        <div className="d-flex align-items-center">
-                                            <ThumbUpRoundedIcon/>
-                                            <span className="rating">{movieDetails.vote_average}</span>
-                                        </div>
-                                    </Typography>
-                                    <Typography className={classes.genres} variant="body2" color="textSecondary"
-                                                component="p">
-                                        {movieDetails.genres.map(genre => <span> {genre.name}</span>)}
-                                    </Typography>
+    return (
+        <>
+            {movie && <div>
+                {/*<CardMedia*/}
+                {/*    className={classes.media}*/}
+                {/*    image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}*/}
+                {/*    title={`${movie.original_title}`}*/}
+                {/*/>*/}
+                <CardContent>
+                    {/*<div className={classes.trailer}>*/}
+                    {/*<iframe width="100%" height="100%"*/}
+                    {/*        src={`https://www.youtube.com/embed/59fc956792514113bb02d590`}>*/}
+                    {/*</iframe>*/}
+                        <YouTube videoId={movieDetails.videos.results[0].key} className={classes.trailer}/>
+                    {/*</div>*/}
+                    <Box fontStyle="italic" fontSize={"1.5rem"}>
+                        <Typography variant="p" component="p" align="center">
+                            {movieDetails.tagline}
+                        </Typography>
+                    </Box>
+                    <Typography variant="p" component="p" align="center">
+                        {movieDetails.overview}
+                    </Typography>
+                    <CardActions disableSpacing>
+                        <div className="d-flex align-items-center justify-content-around card-bottom">
+                            <Typography variant="body4" color="textSecondary" component="p">
+                                <div className="d-flex align-items-center">
+                                    <ThumbUpRoundedIcon/>
+                                    <span className="rating">{movieDetails.vote_average}</span>
                                 </div>
-                            </CardActions>
+                            </Typography>
+                            <Typography className={classes.genres} variant="body2" color="textSecondary"
+                                        component="p">
+                                {movieDetails.genres.map(genre => <span> {genre.name}</span>)}
+                            </Typography>
+                        </div>
+                    </CardActions>
 
-                            { movieDetails.similar.results.length > 0 && <Typography paragraph>Similar Movies</Typography>}
-                            <RelatedMovies relatedMovies={movieDetails.similar.results}/>
-                            {movieDetails.reviews.results.length>0 && <Typography paragraph>Reviews</Typography>}
-                            <div className="mb-4">
-                                {movieDetails.reviews.results.map(({author, content}) =>
-                                    <Accordion>
-                                        <AccordionSummary
-                                            expandIcon={<ExpandMoreIcon/>}
-                                            aria-controls="panel1a-content"
-                                            id="panel1a-header"
-                                        >
-                                            <Typography className={classes.heading}>{author}</Typography>
-                                        </AccordionSummary>
-                                        <AccordionDetails>
-                                            <Typography>
-                                                {content}
-                                            </Typography>
-                                        </AccordionDetails>
-                                    </Accordion>)}
-                            </div>
-                        </CardContent>
-                </div>}
-            </>
-        );
-    }
+                    {movieDetails.similar.results.length > 0 && <Typography paragraph>Similar Movies</Typography>}
+                    <RelatedMovies relatedMovies={movieDetails.similar.results}/>
+                    {movieDetails.reviews.results.length > 0 && <Typography paragraph>Reviews</Typography>}
+                    <div className="mb-4">
+                        {movieDetails.reviews.results.map(({author, content}) =>
+                            <Accordion>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon/>}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <Typography className={classes.heading}>{author}</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        {content}
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>)}
+                    </div>
+                </CardContent>
+            </div>}
+        </>
+    );
+}
 ;
 
 export default MovieExpanded;
