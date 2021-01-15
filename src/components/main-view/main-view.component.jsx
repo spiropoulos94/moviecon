5f5a1ee6d93089c5d8915b1ce3cb99015f2855b3
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-01-15 11:34:48 +0200

    fixed page problem showing only first page

--- src/components/main-view/main-view.component.jsx
+++ src/components/main-view/main-view.component.jsx
@@ -4,13 +4,14 @@ import Jumbotron from "../jumbotron/jumbotron.component";
 import MovieList from "../movielist/movielist.component";
 import SearchInput from "../searchInput/search-input.component";
 import {useInfiniteQuery} from "react-query";
+import Button from "@material-ui/core/Button";
 
 const MainView = () => {
 
 
     const [content, setContent] = useState("now_playing")
 
-    const [latestMoviesPage, setLatestMoviesPage] = useState(2)
+    const [latestMoviesPage, setLatestMoviesPage] = useState(1)
     const [searchResultsPage, setSearchResultsPage] = useState(1)
     const [searchQuery, setSearchQuery] = useState("potter")
 
@@ -20,14 +21,14 @@ const MainView = () => {
         return data
     }
 
-    const fetchMoviesBySearch = async (searchQuery, page) => {
+    const fetchMoviesBySearch = async (searchQuery, searchResultsPage) => {
         const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=bc50218d91157b1ba4f142ef7baaa6a0&language=en-US&query=${searchQuery}&page=${searchResultsPage}&include_adult=false`)
         const data = await res.json();
         return data
     }
 
     const {
-        data:movies
+        data:movies,fetchNextPage
     } = useInfiniteQuery(`latestMovies_page_${latestMoviesPage}`, (page) => fetchLatestMovies(latestMoviesPage),
         {
             getNextPageParam: ((lastPage) => {
@@ -40,7 +41,7 @@ const MainView = () => {
     const {
         data:searchResults
 
-    } = useInfiniteQuery(`movies_results_for_${searchQuery}`, (page) => fetchMoviesBySearch(searchQuery),
+    } = useInfiniteQuery(`movies_results_for_${searchQuery}`, (page) => fetchMoviesBySearch(searchQuery, searchResultsPage),
         {
             getNextPageParam: ((lastPage) => {
                 console.log("last page is", lastPage)
@@ -55,11 +56,14 @@ const MainView = () => {
 
     return (
         <div>
+            <>
             <Jumbotron/>
             <div className="container my-5">
                 <SearchInput setContent={setContent} setSearchQuery={setSearchQuery} />
             </div>
             { content=="now_playing" ? <MovieList data={movies}/> : <MovieList data={searchResults}/> }
+            </>
+            <Button onClick={(latestMoviesPage)=>fetchNextPage(latestMoviesPage)} >Load More</Button>
         </div>
     );
 };
